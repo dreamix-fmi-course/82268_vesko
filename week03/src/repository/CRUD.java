f2847aec0549ba3f4acad0a5df7fa231789e8ec1
Veselin Antonov <vesko2345@gmail.com>
2022-03-31 20:51:29 +0300

    Added solutions for week 3 tasks

---
+++ week03/src/repository/CRUD.java
@@ -0,0 +1,21 @@
+package repository;
+
+import model.Book;
+
+import java.util.List;
+
+public interface CRUD
+{
+
+    void add(Book book);
+
+    Book update(Book book);
+
+    void remove(String isbn);
+
+    Book getByKey(String isbn);
+
+    List<Book> getAll();
+
+    void clear();
+}
